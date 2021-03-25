bd084a7fb536a15d99a33ced3533e217604cac3a
Zach Frohardt <zfrohardt@gmail.com>
2021-03-25 09:19:50 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+document.getElementById('main').remove();
+
+let newHeader = document.createElement('h1');
+newHeader.setAttribute('id', 'victory');
+newHeader.innerHTML = "is the champion";
